eaa807297f351759fa38383d35efdbf5bebd4b09
4thevisionDonghyun <donghyun@4thevision.com>
2023-01-17 19:24:24 +0900

    person 클릭시 answer 나오게 변경

--- src/Pages/FileUpload.js
+++ src/Pages/FileUpload.js
@@ -324,13 +324,12 @@ const FileUpload = () => {
                                                                     .map(([questionTitle, person]) => (
                                                                         <p className="title">
                                                                             <button onClick={toggleClassOnSibling} className="title">{questionTitle}</button>
-                                                                            <div className="person">
-                                                                                {Object.entries(person).map(([key, value]) => (
-                                                                                    <div className="flex">
-                                                                                        <span className="title">{key} : {value}</span>
-                                                                                    </div>
-                                                                                ))}
-                                                                            </div>
+                                                                            {Object.entries(person).map(([personTitle, answer]) => (
+                                                                                <div className="person">
+                                                                                    <button onClick={toggleClassOnSibling} className="title">{personTitle}</button>
+                                                                                    <div className="title answer">{answer}</div>
+                                                                                </div>
+                                                                            ))}
                                                                         </p>
                                                                     ))}
                                                             </div>
