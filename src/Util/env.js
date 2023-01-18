405927b9f3a5e534b096a031eccb1182eb0b39a2
4thevisionDonghyun <donghyun@4thevision.com>
2023-01-18 17:21:35 +0900

    AXIOS OPTION 수정

--- src/Util/env.js
+++ src/Util/env.js
@@ -1,24 +1,30 @@
 
 import {getCookie} from "./cookie";
+import axios, { AxiosRequestConfig } from 'axios';
 
 
 // export const SERVER_URL = "http://192.168.0.13:10000/api/project/"
-export const SERVER_URL = "http://localhost:10000/api/"
+// export const SERVER_URL = "http://localhost:10000/api/"
 // export const SERVER_DICT_URL = "http://localhost:10000/api/dict/";
 
 // export const SERVER_URL = "https://477a-211-221-71-139.jp.ngrok.io/api/project/";
-// export const SERVER_URL = "https://477a-211-221-71-139.jp.ngrok.io/api/";
+export const SERVER_URL = "https://477a-211-221-71-139.jp.ngrok.io/api/";
 
 const userid = getCookie('user_id')
 
-export const AXIOS_OPTION = {
-    withCredentials:true,
-    headers: {
-        'X-AUTH-TOKEN': getCookie('X-AUTH-TOKEN')
-    }
-};
+export const headers = {
+    'X-AUTH-TOKEN': getCookie('X-AUTH-TOKEN')
+}
 
+export const AXIOS_OPTION = Object.assign({}, {
+    withCredentials: true,
+    headers: headers
+})
 
+// export const AXIOS_OPTION = {...{
+//         withCredentials: true,
+//         headers: headers
+//     }}
 
 // export const COOKIE_DOMAIN = "http://192.168.0.13:10000/api/project/";
 // export const COOKIE_DOMAIN = "http://localhost:10000/api/project/";
