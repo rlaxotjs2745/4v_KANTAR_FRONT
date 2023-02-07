9ee9d7ac590033d39de20349aeb8321034688ab2
4thevisionDonghyun <donghyun@4thevision.com>
2023-02-07 17:25:25 +0900

    필터 리셋에 키워드 추가

--- src/Pages/ProjectDetail.js
+++ src/Pages/ProjectDetail.js
@@ -1187,6 +1187,7 @@ const ProjectDetail = () => {
 
         setProjectDetailListFilterOrigin([]) // 서브챕터 라벨 리스트 초기화
         setProjectDetailListFilterOrigin2([]) // 질문 필터 라벨 리스트 초기화
+        setSelectedDictDataR([]) // 키워드 선택 초기화
 
         setSelectedFilter('') // 필터프리셋 선택 해제
         toastNoticeSuccess('필터를 초기화 하였습니다.')
@@ -1614,7 +1615,6 @@ const ProjectDetail = () => {
             setProjectDetailList(projectDetailListOrigin.filter(item => selectedLabelsChapters.includes(item.chapter)))
         }
     },[chapterSubmitHandle])
-
 
     useEffect(()=>{
         if (checkBoxCount3 > 0 && checkBoxCount2 > 0 && checkBoxCount > 0) {
