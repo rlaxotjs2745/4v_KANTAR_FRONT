34493b7e8769d123242b14d0f86bc95d065a59ba
color000 <backgroundcolorfff@gmail.com>
2023-03-06 18:34:01 +0900

    report/create_report 에러 메세지 수정

--- src/Pages/FileUpload.js
+++ src/Pages/FileUpload.js
@@ -113,7 +113,7 @@ const FileUpload = () => {
                 navigate('/')
                 // toastNoticeSuccess('기본 리포트가 생성되었습니다', `/report_detail/1`)
             } else {
-                toastNoticeError('에러가 발생했습니다.')
+                toastNoticeError(res.data.msg)
             }
         }).catch(err => {
             console.log(err);
